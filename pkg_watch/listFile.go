2d80909c2766a9d008d2ce2947d2105979318366
Nimit <nimit.jyotiana@hotmail.com>
2022-01-28 17:34:31 +0530

    update: package info in json
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- pkg_watch/listFile.go
+++ pkg_watch/listFile.go
@@ -8,15 +8,16 @@ import (
     "regexp"
     "path/filepath"
     "gopkg.in/ini.v1"
-    //"github.com/go-ini/ini"
+    "encoding/json"
+    "io/ioutil"
 )
 
 func main() {
 
 	// process arguments
-	cmdPtr := flag.String("cmd", "pkg_list", "command to p/rforr")
+	cmdPtr := flag.String("cmd", "list", "command/action to perform e.g. list,")
 	repoPtr := flag.String("repo", "./", "path of source code repositroy")
-	targetPtr := flag.String("target", "linux", "platform e.g. linux, linux2, windows")
+	targetPtr := flag.String("target", "linux", "platform e.g. linux, windows, all")
 
 	flag.Parse()
 
@@ -30,35 +31,48 @@ func main() {
 	log.SetOutput(file)
 	*/
 
-	log.Println("INFO log Start")
+	log.Println("INFO log start")
+	log.Printf("INFO command [%s] \n", *cmdPtr)
 	log.Printf("INFO path of repository [%s] \n", *repoPtr)
 	log.Printf("INFO platform target [%s] \n", *targetPtr)
 
-	if (strings.Compare(*cmdPtr, "pkg_list") == 0) {
-		var list, err = getPlanfileList(*repoPtr)
+	if (strings.Compare(*cmdPtr, "list") == 0) {
+		// get list of planfiles from repo
+		var list, err = getPlanfileList(*repoPtr, *targetPtr)
 		if (err != nil) {
 			log.Printf("ERR unable to get planfiles [%s] \n", err)
 		}
 
+		// get parameter from planfile
 		getPackageData(list)
 
 	} else {
 		log.Println("ERR invalid command")
 	}
+
+	log.Println("INFO log stop")
 }
 
-func getPlanfileList(dirpath string) ([]string, error) {
+func getPlanfileList(dirpath, target string) ([]string, error) {
 	var files []string
 
 	// get list of all planfile (plan.sh & plan.ps1) present in directory
 	err := filepath.Walk(dirpath,
 		func(path string, info os.FileInfo, err error) error {
+			// skip ".deprecated" directory
+			if (info.IsDir() == true && info.Name() == ".deprecated") {
+				return filepath.SkipDir
+			}
+
 			var r, _ = regexp.Compile("plan\\.sh|plan\\.ps1")
 			if (info.IsDir() == false && r.MatchString(path) == true) {
-				// remove deprecated, test planfiles
-				if(strings.Contains(path, ".deprecated") == false) {
-					files = append(files, path)
+				// remove test planfiles
+
+				// exclude non target specific planfile
+				if(strings.Compare(target, "linux") == 0) {
 				}
+
+				files = append(files, path)
 			}
 			return nil
 		})
@@ -67,29 +81,66 @@ func getPlanfileList(dirpath string) ([]string, error) {
 }
 
 func getPackageData(list []string) {
+	type Package struct {
+		Planfile string `json:"planfile"`
+		Origin string `json:"pkg_origin"`
+		Name string `json:"pkg_name"`
+		Version string `json:"pkg_version"`
+		Source string `json:"pkg_source"`
+	}
+
+	var PackageList []Package
+
+	// name of parameter to read from planfile
+	//var param = [4]string{"pkg_origin", "pkg_name", "pkg_version", "pkg_source"}
+
 	// read planfile and fetch plan meta data
-	for _, element := range list {
+	for _, planfile := range list {
 		var cfg, err = ini.LoadSources(ini.LoadOptions{
 			SkipUnrecognizableLines: true,
-			}, element)
+			}, planfile)
 
 		if (err != nil) {
-			log.Printf("ERR fail to load planfile [%s], err[%s] \n", element, err)
+			log.Printf("ERR fail to load planfile [%s], err[%s] \n", planfile, err)
 		}
 
-		// list of parameters to be read from planfile
-		var key = ""
-		var param = [4]string{"pkg_origin", "pkg_name", "pkg_version", "pkg_source"}
+		var item Package
 
-		for _, key = range param {
-			if(strings.Contains(element, "plan.ps1") == true) {
-				key = "$" + key
-			}
-
-			// Classic read of values, default section can be represented as empty string
-			log.Printf("[%s] [%s] \n", key, cfg.Section("").Key(key).String())
+		// Classic read of values, default section can be represented as empty string
+		if(strings.Contains(planfile, "plan.ps1") == true) {
+			// windows planfile
+			item = Package{Origin: cfg.Section("").Key("$pkg_origin").String(),
+				Name: cfg.Section("").Key("$pkg_name").String(),
+				Version: cfg.Section("").Key("$pkg_version").String(),
+				Source: cfg.Section("").Key("$pkg_source").String()}
+		} else {
+			// linux planfile
+			item = Package{Origin: cfg.Section("").Key("pkg_origin").String(),
+				Name: cfg.Section("").Key("pkg_name").String(),
+				Version: cfg.Section("").Key("pkg_version").String(),
+				Source: cfg.Section("").Key("pkg_source").String()}
 		}
 
-		log.Printf("planfile [%s] \n", element)
+		// update planfile name
+		item.Planfile = planfile
+
+		// add to list
+		PackageList = append(PackageList, item)
 	}
+
+	// convert list of JSON
+	jsonString, err := json.Marshal(PackageList)
+	if(err != nil) {
+		log.Printf("ERR failed to encode json; [%s]", err)
+		return
+	}
+
+	// delete content of file
+	err = os.Truncate("packagelist.json", 0)
+	if(err != nil) {
+		log.Println("ERR failed to delete content of packagelist.json; [%s]", err)
+	}
+
+	// write to file
+	ioutil.WriteFile("packagelist.json", jsonString, 0644)
 }
