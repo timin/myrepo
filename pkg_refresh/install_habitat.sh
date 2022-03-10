fa25c21f09f1f7d2d37669e7add4f00b174d0183
Nimit <nimit.jyotiana@hotmail.com>
2022-03-10 13:04:32 +0530

    Update install_habitat.sh

--- pkg_refresh/install_habitat.sh
+++ pkg_refresh/install_habitat.sh
@@ -3,6 +3,11 @@
 set -eou pipefail
 
 main() {
+	setupHabitat
+	setupPackageRefresh
+}
+
+setupHabitat() {
 	# update ubuntu packages
 	sudo apt update
 	sudo apt upgrade -y
@@ -15,8 +20,6 @@ main() {
 
 	# configure habitat
 	curl https://raw.githubusercontent.com/timin/myutil/main/pkg_refresh/conf/cli.toml --output /home/ubuntu/.hab/etc/cli.toml
-	
-	exit
 	
 	# set ssl certificate
 	curl https://raw.githubusercontent.com/timin/myutil/main/pkg_refresh/conf/ssl_certificate.pem --output /home/ubuntu/.hab/cache/ssl/cert.pem
@@ -32,10 +35,9 @@ main() {
 	cd /home/ubuntu/Refresh/
 	
 	# hab studio(or plan builder) ready for use
+}
 
-  exit
-
-	# clone repo
+setupPackageRefresh() {
 }
 
 main "$@" || exit 99
