37b915963187e0bfa33757ebe5929f8638b03f43
Nimit <nimit.jyotiana@hotmail.com>
2022-03-10 12:47:01 +0530

    Update install_habitat.sh

--- pkg_refresh/install_habitat.sh
+++ pkg_refresh/install_habitat.sh
@@ -18,6 +18,12 @@ main() {
 	
 	# set ssl certificate
 	curl https://raw.githubusercontent.com/timin/myutil/main/pkg_refresh/conf/ssl_certificate.pem --output /home/ubuntu/.hab/cache/ssl/cert.pem
+	
+	# download public key from BLDR
+	hab origin key download core
+	
+	# download public key from BLDR
+	hab origin key download core --secret
 
   exit
 
