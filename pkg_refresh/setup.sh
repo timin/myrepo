7867d185687e9695dfd087a80c136d13e9024526
Nimit <nimit.jyotiana@hotmail.com>
2022-03-10 16:18:58 +0530

    updated setup script
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- pkg_refresh/setup.sh
+++ pkg_refresh/setup.sh
@@ -3,21 +3,60 @@
 set -eou pipefail
 
 main() {
-	setupHabitat
-	setupPackageRefresh
+	for a in "$@";
+	do
+		case $a in
+			-i=*|--install=*)
+			INSTALL="${a#*=}"
+			shift # past argument=value
+			;;
+			-s=*|--setup=*)
+			SETUP="${a#*=}"
+			shift # past argument=value
+			;;
+			-*|--*)
+			echo "Unknown option $a"
+			exit 1
+			;;
+			*)
+			;;
+		esac
+	done
+
+	if [[ $INSTALL == "linux" ]];
+	then
+		# install hab for linux
+		installHabitat "x86_64-linux"
+	elif [[$INSTALL == "linux2" ]];
+	then
+		# install hab for linux2
+		installHabitat "x86_64-linux-kernel2"
+	fi
+
+	if [[ $SETUP == "" ]];
+	then
+		# configure habitat
+		setupHabitat
+	elif [[ $SETUP == "" ]];
+	then
+		# configure package refresh
+		setupPackageRefresh
+	fi
 }
 
-setupHabitat() {
+installHabitat() {
 	# update ubuntu packages
 	sudo apt update
 	sudo apt upgrade -y
 
 	# install habitat program
-	curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash -s -- -t x86_64-linux-kernel2
+	curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash -s -- -t $type
 
 	# set habitat license
 	hab license accept
+}
 
+setupHabitat() {
 	# configure habitat
 	curl https://raw.githubusercontent.com/timin/myutil/main/pkg_refresh/conf/cli.toml --output /home/ubuntu/.hab/etc/cli.toml
 	
