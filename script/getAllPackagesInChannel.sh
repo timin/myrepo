d93b37752c59cc6ee3f4621050ceb93fa5fe06f8
Nimit <nimit.jyotiana@hotmail.com>
2022-08-04 15:45:29 +0530

    added script to fetch packages from channel
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

---
+++ script/getAllPackagesInChannel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PKG_ORIGIN="core"
+PKG_CHANNEL="refresh_xlib"
+
+
+INDEX_START=0
+INDEX_END=1
+FILE="channel_packages.txt"
+
+# delete file if present
+rm -f $FILE
+
+# fetch packages from BLDR channel
+while [ "$INDEX_START" -lt "$INDEX_END" ]
+do
+	result=""
+	result="$(curl -s "https://bldr.habitat.sh/v1/depot/channels/${PKG_ORIGIN}/${PKG_CHANNEL}/pkgs?range=${INDEX_START}")"
+
+	INDEX_START=$(echo ${result} | jq -r '.range_end')+1
+	((INDEX_START=INDEX_START+1))
+	INDEX_END=$(echo ${result} | jq -r '.total_count')
+
+	echo ${result} | jq -r '.data[] | .origin + "\/" + .name + "\/" + .version + "\/" + .release' >> $FILE
+done
+
+echo "Total package fetched : $INDEX_END"
