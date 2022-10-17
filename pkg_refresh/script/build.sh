ce6d5ba48c27ff1c71ab09ca1b5e7e4dd95d1f51
Nimit <nimit.jyotiana@hotmail.com>
2022-10-17 15:23:19 +0530

    Create build.sh

---
+++ pkg_refresh/script/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source "./refresh.rc"
+
+
+# file which has list of packages to be build
+PackageList="list.txt"
+# path of packages directory
+PackagePath="bootstrap-plans/"
+LogFile="build.log"
+
+function log {
+	echo `date "+%Y-%m-%d %H:%M:%S"`" $1" >> $LogFile
+}
+
+while IFS= read -r line; do
+	# tokenize line
+	IFS=' '
+	read -a arr <<< "$line"
+	#len="${#arr[@]}"
+	plan="${arr[0]}"
+	args="${arr[1]}"
+
+	if [[ "$args" == "skip" ]];
+	then
+		log "[$plan] Skip with args [$args]"
+		continue
+	else
+		log "[$plan] Build with args [$args]"
+	fi	
+
+	# build command
+	cmd="hab pkg build -N $PackagePath"
+	cmd="${cmd}$plan"
+
+	log "[$plan] Building [$cmd]"
+
+	start=$(date +%s)
+	eval "$cmd"
+	end=$(date +%s)
+
+	if [[ $? != 0 ]];
+	then
+		log "[$plan] ERR Built failed"
+		break
+	fi
+
+	log "[$plan] Built in [$(($end-$start))] seconds"	
+
+done < $PackageList
+
+exit 1
