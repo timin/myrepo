2490f4293c5893aa8e7aaee43edab7246c305c2e
Nimit <nimit.jyotiana@hotmail.com>
2023-08-16 17:16:02 +0000

    update build sequence generator
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- generate_build_order.sh
+++ generate_build_order.sh
@@ -2,27 +2,25 @@
 
 # a script to generate build order based on build-order used in last refresh.
 # file_bo - file has build order which had worked in last refresh
-# file_jumble - file having jumbled set of packages needs to order for build
-# file_order - resultant set of package order as per last Build Order
+# file_input - file having jumbled set of packages needs to be arranged in order for build
+# file_output - resultant set of package order as per last Build Sequence
 
-file_bo="bo.txt"
-file_jumble="rdep.txt"
-file_order="order.txt"
+file_bo="build_order"
+file_input="rdep"
+file_output="build_sequence"
 
 # delete already existing file
-`rm -f $file_order`
+$(rm -f $file_output)
 
 # iterate through file "file_bo" and if package is present in unsorted list write to final file
-while IFS= read -r line;
-do
+while IFS= read -r line; do
 	#echo "$line"
 
-	if grep -Fxq "$line" $file_jumble
-	then
+	if grep -Fxq "$line" $file_input; then
 		# if found
 		echo "Found [$line] in build order"
-		echo "$line" >> $file_order
+		echo "$line" >> $file_output
 	fi
 done < "$file_bo"
 
-echo "Build order is generated, check file [$file_order]\n"
+echo "Build sequence is generated, check file [$file_output]\n"
